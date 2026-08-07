6a12e68238eff3f96e3907f848b6082143dd33f3
Praise Mbeya <mbeyachawanangwapraise.133934@marwadiuniversity.ac.in>
2026-08-07 15:00:08 +0530

    Add files via upload

---
+++ programten.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class programten {
+
+     public static void main (String args[]) {
+     Scanner scanner = new Scanner(System.in);
+
+int scores[]= new int [5];
+
+
+for (int i = 0; i < scores.length; i++) {
+
+    System.out.print("\nEnter 5 scores : ");
+    scores[] = scn.nextInt();
+
+}
+
+  System.out.print("Array elements are : ");
+
+   for (int i = 0; i < scores.length; i++) {
+	System.out.print(scores[i]);
+        }
+    }
+}
